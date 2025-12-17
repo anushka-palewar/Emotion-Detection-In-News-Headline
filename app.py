1094e91ae5a7893e97ef1833d1e5906f3c55abf4
anushka-palewar <anushkapalewar81@gmail.com>
2025-12-17 15:34:14 +0530

    Emotion detection ML project

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+# Load full pipeline (vectorizer + model)
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
+
+    if request.method == "POST":
+        text = request.form["headline"]
+        prediction = model.predict([text])[0]
+
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
